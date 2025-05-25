44ba5e18e9c9ddc9545f9d4bfffd48a433fc08a1
TacoConKvass <e_frun@o2.pl>
2025-05-25 17:18:57 +0200

    [Fix:ML] Package scripts

--- package.sh
+++ package.sh
@@ -1,9 +1,9 @@
-@echo off
+rm -rf package
+mkdir package
+
 dotnet publish
 
-mkdir package
 cd bin/Release/publish
 tar -a -cf ../../../package/RL2.ModLoader.zip package 
 tar -a -cf ../../../package/RL2.ModLoader-manual.zip manual
 echo Packaged RL2.ModLoader
-cd ../../..
