2e0b53b6ed821ced853c956674e6da218d2b32d5
TacoConKvass <e_frun@o2.pl>
2025-05-25 17:14:37 +0200

    [Change:ML] Adapt DevSetup 2.1

--- setup.bat
+++ setup.bat
@@ -1,13 +1,8 @@
 @echo off
-curl -OL https://github.com/RL2-API/RL2.ModLoader.DevSetup/releases/download/1.0.0/RL2.ModLoader.DevSetup-v1.0.0.tar.gz
+curl -OL https://github.com/RL2-API/RL2.ModLoader.DevSetup/releases/download/v2.1.0/RL2.ModLoader.DevSetup.tar.gz
 mkdir RL2.ModLoader.DevSetup
-tar -xzvf ./"RL2.ModLoader.DevSetup-v1.0.0.tar.gz" -C ./RL2.ModLoader.DevSetup
-del "RL2.ModLoader.DevSetup-v1.0.0.tar.gz"
-
-curl -OL https://github.com/MonoMod/MonoMod/releases/download/v22.07.31.01/MonoMod-22.07.31.01-net452.zip
-mkdir lib
-tar -xf "MonoMod-22.07.31.01-net452.zip" -C lib
-del "MonoMod-22.07.31.01-net452.zip"
+tar -xzf ./"RL2.ModLoader.DevSetup.tar.gz" -C ./RL2.ModLoader.DevSetup
+del "RL2.ModLoader.DevSetup.tar.gz"
 
 cd RL2.ModLoader.DevSetup
 RL2.ModLoader.DevSetup.exe
