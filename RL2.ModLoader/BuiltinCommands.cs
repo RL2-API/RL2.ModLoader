8c9949902c323bd751e79e01345f3d1d8ea174a3
TacoConKvass <e_frun@o2.pl>
2025-06-25 21:10:47 +0200

    [Fix] Utility build task (again)

--- RL2.ModLoader/BuiltinCommands.cs
+++ RL2.ModLoader/BuiltinCommands.cs
@@ -180,26 +180,29 @@ public static class BuiltinCommands
 					</Reference>
 				</ItemGroup>
 
-				<!-- Utility build task -->
-				<Target Name="CopyToMods" AfterTargets="PostBuildEvent">
+				<!-- Utility build tasks -->
+				<Target Name="Package" AfterTargets="PostBuildEvent">
 					<ItemGroup>
-						<Compiled Include="$(TargetDir)$(AssemblyName)*" />
-						<ModJson Include="$(ProjectDir)*.mod.json" />
-						<JsonData Include="$(ProjectDir)**\*.json" Exclude="$(ProjectDir)**\obj\**\*.*;$(ProjectDir)**\Properties\**\*.*"/>
-						<PNGs Include="$(ProjectDir)**\*.png"/>
+						<Compiled Include="$(TargetDir)\$(AssemblyName).*" />
+						<ModJson Include="$(ProjectDir)\*.mod.json"/>
+						<JsonData Include="$(ProjectDir)**\*.json" Exclude="$(BaseIntermediateOutputPath)\**\*;**\Properties\*;$(TargetDir)\**\*" />
+						<PNGs Include="$(ProjectDir)**\*.png" Exclude="$(TargetDir)\**\*"/>
 					</ItemGroup>
-
-					<Message Text="Copying $(AssemblyName) to the Mods directory..." Importance="high" />
-					<Copy SourceFiles="@(Compiled)" DestinationFolder="$(RL2_ModsPath)\$(AssemblyName)" />
-					<Copy SourceFiles="@(ModJson)" DestinationFolder="$(RL2_ModsPath)\$(AssemblyName)" />
-					<Copy SourceFiles="@(JsonData)" DestinationFolder="$(RL2_ModsPath)\$(AssemblyName)\%(RecursiveDir)" />
-					<Copy SourceFiles="@(PNGs)" DestinationFolder="$(RL2_ModsPath)\$(AssemblyName)\%(RecursiveDir)" />
 
 					<Message Text="Preparing $(AssemblyName)'s package folder..." Importance="high" />
 					<Copy SourceFiles="@(Compiled)" DestinationFolder="$(TargetDir)\$(AssemblyName)" />
 					<Copy SourceFiles="@(ModJson)" DestinationFolder="$(TargetDir)\$(AssemblyName)" />
 					<Copy SourceFiles="@(JsonData)" DestinationFolder="$(TargetDir)\$(AssemblyName)\%(RecursiveDir)" />
 					<Copy SourceFiles="@(PNGs)" DestinationFolder="$(TargetDir)\$(AssemblyName)\%(RecursiveDir)" />
+				</Target>
+
+				<Target Name="CopyToMods" AfterTargets="Package">
+					<ItemGroup>
+						<Package Include="$(TargetDir)\$(AssemblyName)\**\*" />
+					</ItemGroup>
+
+					<Message Text="Copying $(AssemblyName) to the Mods directory..." Importance="high" />
+					<Copy SourceFiles="@(Package)" DestinationFolder="$(RL2_ModsPath)\$(AssemblyName)\%(RecursiveDir)" />
 				</Target>
 			</Project>
 			"""
