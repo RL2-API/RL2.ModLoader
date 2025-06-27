e6bbbc46e57580db06606e18c1d1b2a946902dd0
TacoConKvass <e_frun@o2.pl>
2025-06-27 16:12:26 +0200

    [Change] Integrate the packaging script into MSBuild

--- package.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-dotnet publish
-
-mkdir package
-cd bin/Release/publish
-tar -a -cf ../../../package/RL2.ModLoader.zip package 
-tar -a -cf ../../../package/RL2.ModLoader-manual.zip manual
-echo Packaged RL2.ModLoader
-cd ../../..
--- package.sh
+++
@@ -1,9 +0,0 @@
-rm -rf package
-mkdir package
-
-dotnet publish
-
-cd bin/Release/publish
-tar -a -cf ../../../package/RL2.ModLoader.zip package 
-tar -a -cf ../../../package/RL2.ModLoader-manual.zip manual
-echo Packaged RL2.ModLoader
