9965d8e0b1448e3b28743afb006dad67c3799b03
TacoConKvass <e_frun@o2.pl>
2026-01-28 15:57:12 +0100

    Add: Utility

---
+++ RL2.ModLoader/Usings.cs
@@ -0,0 +1,18 @@
+#pragma warning disable IDE0005
+#pragma warning disable CS8981
+
+global using u8 = byte;
+global using u16 = ushort;
+global using u32 = uint;
+global using u64 = ulong;
+
+global using i8 = sbyte;
+global using i16 = short;
+global using i32 = int;
+global using i64 = long;
+
+global using f32 = float;
+global using f64 = double;
+
+global using isize = nint;
+global using usize = nuint;
